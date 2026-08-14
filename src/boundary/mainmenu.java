5a3faa7050153a1038a5727014eb1cc1ea1bcde1
Han <hanyean282@gmail.com>
2026-08-14 21:16:28 +0800

    refactor(housekeeping): clean up unused report classes and add status history log

--- src/boundary/mainmenu.java
+++ src/boundary/mainmenu.java
@@ -38,7 +38,7 @@ public class mainmenu {
 
             switch (choice) {
                 case 1:
-                    new HousekeepingUI().start();
+                    new HousekeepingUI(scanner).start();
                     break;
                 case 2:
                 case 3:
